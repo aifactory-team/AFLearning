e985bd9b110aa85c726ff82ce9b3da17c8f78e2d
Taeyoung Kim <adam.tykim@gmail.com>
2022-09-23 19:15:27 +0900

    Add files via upload

---
+++ 2137/etth1_af.sh
@@ -0,0 +1,24 @@
+# add --individual for DLinear-I
+if [ ! -d "./logs" ]; then
+    mkdir ./logs
+fi
+
+if [ ! -d "./logs/LongForecasting" ]; then
+    mkdir ./logs/LongForecasting
+fi
+seq_len=336
+model_name=DLinear
+
+python -u run_longExp.py \
+  --is_training 1 \
+  --root_path ./dataset/ \
+  --data_path ETTh1.csv \
+  --model_id ETTh1_$seq_len'_'720 \
+  --model $model_name \
+  --data ETTh1 \
+  --features M \
+  --seq_len $seq_len \
+  --pred_len 720 \
+  --enc_in 7 \
+  --des 'Exp' \
+  --itr 1 --batch_size 32 --learning_rate 0.005 >logs/LongForecasting/$model_name'_'Etth1_$seq_len'_'720.log
